80f069b829876f69433e3054a8b8ac623d67e570
kerenle2 <33980896+kerenle2@users.noreply.github.com>
2018-06-19 17:14:47 +0300

    try

--- app/src/main/java/com/example/efrat/myapplication/ImageService.java
+++ app/src/main/java/com/example/efrat/myapplication/ImageService.java
@@ -2,12 +2,20 @@ package com.example.efrat.myapplication;
 
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.widget.Toast;
 
 public class ImageService extends Service {
-
+    private BroadcastReceiver broadcastReceiver;
+    private TcpClient client;
     public ImageService() {}
 
     @Override
@@ -18,6 +26,7 @@ public class ImageService extends Service {
     @Override
     public void onCreate()
     {
+        this.client = new TcpClient();
         super.onCreate();
         //put here code for the service
     }
@@ -26,6 +35,8 @@ public class ImageService extends Service {
     {
         Toast.makeText(this, "Service starting...",
                 Toast.LENGTH_SHORT).show();
+        connectBroadcast();
+
         return START_STICKY;
     }
 
@@ -34,4 +45,29 @@ public class ImageService extends Service {
         Toast.makeText(this, "Service ending...",
                 Toast.LENGTH_SHORT).show();
     }
+    public void connectBroadcast(){
+        final IntentFilter filter = new IntentFilter();
+        filter.addAction("android.net.wifi.supplicant.CONNECTION_CHANGE");
+        filter.addAction("android.net.wifi.STATE_CHANGE");
+        this.broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+                NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+                if(networkInfo != null){
+                    if(networkInfo.getType() == ConnectivityManager.TYPE_WIFI){
+                        //get the different network states
+                        if(networkInfo.getState() == NetworkInfo.State.CONNECTED){
+                           //start the transfer from DCIM!!!
+                           client.SendAllPicsToServer();
+                       }
+                    }
+                }
+            }
+        };
+        // Registers the receiver so that your service will listen for broadcasts
+
+        this.registerReceiver(this.broadcastReceiver, filter);
+    }
+
 }
--- app/src/main/java/com/example/efrat/myapplication/TcpClient.java
+++ app/src/main/java/com/example/efrat/myapplication/TcpClient.java
@@ -2,15 +2,22 @@ package com.example.efrat.myapplication;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class TcpClient {
 
     private DirectoryHandler directoryHandler;
 
+    public TcpClient(){
+        this.directoryHandler = new DirectoryHandler();
+    }
     public void SendAllPicsToServer()
     {
         File[] pics = directoryHandler.getImages();
@@ -29,15 +36,42 @@ public class TcpClient {
         {
             FileInputStream fis = new FileInputStream(pic);
             Bitmap bm = BitmapFactory.decodeStream(fis);
-            byte[] imgbyte = ConvertPicToByte(bm);
+            final byte[] imgbyte = ConvertPicToByte(bm); // if its not final it doesnt work
+
+            //now send the pic to the server!!!!
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                   try{
+                        InetAddress serverAddr = InetAddress.getByName("10.0.0.2");
+                        Socket socket = new Socket(serverAddr,8000 ); // check port number
+
+
+                        try {
+                            //send msg to server
+                            OutputStream output = socket.getOutputStream();
+                            // FileInputStream fis = new FileInputStream(pic);
+                            output.write(imgbyte);
+                            output.flush();
+                        }
+                        catch (Exception e){
+                            Log.e("TCP", "S: Error", e);
+                        }finally {
+                            socket.close();
+                        }
+                    }catch (Exception e){
+                        Log.e("TCP", "S: Error", e);
+                    }
+                }
+            });
+            thread.start();
+
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
-
-        //now send the pic to the server!!!!
-
     }
 
     public byte[] ConvertPicToByte(Bitmap bitmap)
