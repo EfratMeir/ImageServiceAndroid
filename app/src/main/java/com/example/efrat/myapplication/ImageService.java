6c8ba2198f48bddab942a1886050681d2c7962eb
EfratMeir <efratoosh44@gmail.com>
2018-06-21 02:43:22 +0300

    added prograssBar, prograssBarHandler

--- app/src/main/java/com/example/efrat/myapplication/ImageService.java
+++ app/src/main/java/com/example/efrat/myapplication/ImageService.java
@@ -1,12 +1,17 @@
 package com.example.efrat.myapplication;
 
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.widget.Toast;
 
 public class ImageService extends Service {
+
+
 
     public ImageService() {}
 
@@ -26,7 +31,22 @@ public class ImageService extends Service {
     {
         Toast.makeText(this, "Service starting...",
                 Toast.LENGTH_SHORT).show();
+
+        //tryout -  progress bar:
+        final Context context = this;
+        new Thread() {
+
+            @Override
+            public void run() {
+                ProgressBarHandler pbHandler = new ProgressBarHandler(new ProgressBar(50));
+                pbHandler.DisplayProgressBar(context);
+            }
+        }.start();
+        //end tryout
+
         return START_STICKY;
+
+
     }
 
     public void onDestroy()
---
+++ app/src/main/java/com/example/efrat/myapplication/ProgressBarHandler.java
@@ -0,0 +1,73 @@
+package com.example.efrat.myapplication;
+
+
+import android.app.NotificationManager;
+import android.content.Context;
+//import android.support.v4.app.NotificationCompat;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.view.View;
+import android.app.NotificationChannel;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+
+public class ProgressBarHandler {
+    private ProgressBar pb;
+
+    /**
+     * constructor
+     * @param pb - progress bar
+     */
+    public ProgressBarHandler(ProgressBar pb)
+    {
+        this.pb = pb;
+    }
+
+
+    @RequiresApi( api = Build.VERSION_CODES.O)
+    public void DisplayProgressBar(Context context)
+    {
+
+        String channelId = "ImageServiceApp";
+        CharSequence channelName = "ImageServiceAndroid";
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, importance);
+        final NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.createNotificationChannel(notificationChannel);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "ImageServiceApp");
+        builder.setContentTitle("Picture Transfer")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentText("Transfer in progress")
+                .setPriority(NotificationCompat.PRIORITY_LOW);
+
+        while(!pb.isFinished())
+        {
+            builder.setProgress(pb.getLimit(), pb.getTransferd(), false);
+            notificationManager.notify(0, builder.build());
+            try
+            {
+                Thread.sleep(400);
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+
+        builder.setProgress(0, 0, false);
+        builder.setContentText("Transfer Completed. " + pb.getTransferd() + "Images Transferd.");
+        notificationManager.notify(0,builder.build());
+
+    }
+
+
+
+
+}
