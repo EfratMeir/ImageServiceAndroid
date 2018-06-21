f36433aa36016ad46b1a2a1d89a23e17a3eee4aa
kerenle2 <33980896+kerenle2@users.noreply.github.com>
2018-06-21 13:44:37 +0300

    .

--- app/src/main/java/com/example/efrat/myapplication/TcpClient.java
+++ app/src/main/java/com/example/efrat/myapplication/TcpClient.java
@@ -60,7 +60,7 @@ public class TcpClient {
                     for (File pic : pics)
                     {
                         sendOnePicToserver(pic);
-                        
+
                     }
                     sendEndMsg();
                 }
